d8669efb5116e54bade279944683cc2c9da21957
galibhassan <galib.hassan.0@gmail.com>
2021-11-25 09:10:49 -0500

    Render proper components in ToolDetails

--- react/src/components/toolDetails/toolDetails.component.js
+++ react/src/components/toolDetails/toolDetails.component.js
@@ -1,12 +1,28 @@
 import classes from './toolDetails.module.css'
 import {useSelector} from 'react-redux'
+import { BrushDetail } from '../brushDetails/brushDetails.component'
+import { EyedropperDetails } from '../eyedropperDetails/eyedropperDetails.component'
+import {SaveDetails} from '../saveDetail/saveDetails.component'
+import {RefImageDetails} from '../refImageDetails/refImageDetails.component'
+import {GridDetails} from '../gridDetails/gridDetails.component'
 
 export const ToolDetails = () => {
   const {activeTool} = useSelector(state=>state.toolReducer)
   
+  const renderToolDetails = ()=>{
+    switch(activeTool){
+      case "brush": return <BrushDetail/>;
+      case "eyedropper": return <EyedropperDetails/>;
+      case "save": return <SaveDetails/>;
+      case "refImage": return <RefImageDetails/>;
+      case "grid": return <GridDetails/>;
+      default: return <BrushDetail/>
+    }
+  }
+
   return(
     <div>
-      {activeTool}
+      {renderToolDetails()}
     </div>
   )
 
