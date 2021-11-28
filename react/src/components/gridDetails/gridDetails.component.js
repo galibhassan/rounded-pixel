e190d740d4afb99aa0f0d1cf8b7ad243c12f336f
galibhassan <galib.hassan.0@gmail.com>
2021-11-27 23:18:44 -0500

    Connect roundedness slider to global store, Apply lint

--- react/src/components/gridDetails/gridDetails.component.js
+++ react/src/components/gridDetails/gridDetails.component.js
@@ -1,26 +1,51 @@
 import classes from "./gridDetails.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { drawingActions } from "../../store/drawingSlice";
 
 const TOOL_TITLE = "Grid";
 
 export const GridDetails = () => {
+  const { roundedness } = useSelector((state) => state.drawingReducer);
+  const dispatch = useDispatch();
+
+  const handleRoundednessChange = (e) => {
+    dispatch(
+      drawingActions.setPixelRoundedness({
+        roundedness: parseInt(e.currentTarget.value),
+      })
+    );
+  };
   return (
     <div className={classes.toolWrapper}>
       <div className={classes.toolTitle}>{TOOL_TITLE}</div>
 
       <div className={classes.toolChunkContainer}>
         <div>Pixel Width</div>
-        <input className={classes.textInput + " " + classes.textInput_short} type="number" />
+        <input
+          className={classes.textInput + " " + classes.textInput_short}
+          type="number"
+        />
       </div>
       <div className={classes.toolChunkContainer}>
         <div>Pixel Height</div>
-        <input className={classes.textInput + " " + classes.textInput_short} type="number" />
+        <input
+          className={classes.textInput + " " + classes.textInput_short}
+          type="number"
+        />
       </div>
 
       <button className={classes.buttonStandard}>Regenerate Grid</button>
 
       <div className={classes.toolChunkContainer}>
         <div>Pixel Roundedness</div>
-        <input type="range" className={classes.slider}/>
+        <input
+          type="range"
+          className={classes.slider}
+          onChange={handleRoundednessChange}
+          min={0}
+          max={20}
+        />
+        <div>{roundedness}</div>
       </div>
     </div>
   );
