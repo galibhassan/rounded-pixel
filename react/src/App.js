29bc1ae62c32e3c77ec615bc0d3a4973b8f3443a
galibhassan <galib.hassan.0@gmail.com>
2021-11-28 19:44:04 -0500

    Add contents to App component

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import classes from "./App.module.css";
+import { Logo } from "./components/logo/logo.component";
+import { ToolDetails } from "./components/toolDetails/toolDetails.component";
+import { Board } from "./components/board/board.component";
+import { ToolBar } from "./components/toolbar/toolbar.component";
+import {ToolButton} from "./components/toolButton/toolButton.component"
 
 function App() {
   return (
-    <div className="App">
-      Hello from React
+    <div className={classes.appComponent}>
+      <div className={classes.appContainer}>
+        <div className={classes.appRow_1}>
+          <div className={classes.logoContainer}>
+            <Logo />
+          </div>
+          <div className={classes.toolDetailContainer}>
+            <ToolDetails />
+          </div>
+          <div className={classes.menuContainer}></div>
+        </div>
+        <div className={classes.appRow_2}>
+          <div className={classes.toolBarContainer}>
+            <ToolBar>
+              <ToolButton toolName="brush"/>
+              <ToolButton toolName="eraser"/>
+              <ToolButton toolName="grid"/>
+              <ToolButton toolName="eyedropper"/>
+              <ToolButton toolName="refImage"/>
+              <ToolButton toolName="save"/>
+            </ToolBar>
+          </div>
+          <div className={classes.boardContainer}>
+            <Board parentWidth={700} parentHeight={300} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
