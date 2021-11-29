51c8710089eb8247986935eda2172391d46ebeba
galibhassan <galib.hassan.0@gmail.com>
2021-11-28 21:23:55 -0500

    Improve styling of EyedropperDetails

--- react/src/components/eyedropperDetails/eyedropperDetails.component.js
+++ react/src/components/eyedropperDetails/eyedropperDetails.component.js
@@ -1,10 +1,10 @@
-import classes from "./eyedropperDetails.module.css";
-
+import classesEyedropper from "./eyedropperDetails.module.css";
+import classes from "../../commonStyle/toolDetailsCommon.module.css"
 const TOOL_TITLE = "Eyedropper";
 
 export const EyedropperDetails = () => {
   return (
-    <div className={classes.toolWrapper}>
+    <div className={classes.toolDetailsWrapper}>
       <div className={classes.toolTitle}>{TOOL_TITLE}</div>
     </div>
   );
